08174ee9179b3f17780254a839170954b5ff5035
Samarth AGARWAL <samu.agarwal@gmail.com>
2014-03-29 19:28:14 +0100

    Random edits

--- src/lazygrid.js
+++ src/lazygrid.js
@@ -1,63 +1,68 @@
-(function(){  
+(function() {
   "use strict";
   // Create your component here
   // http://x-tags.org/docs
-  
+
   xtag.register('sam-lazygrid', {
     lifecycle: {
       created: function() {},
       inserted: function() {},
-      removed: function() {},
+      removed: function() {
+        this.destroy();
+      },
       attributeChanged: function() {}
-    }, 
-    events: { 
-      scroll: function () {
-      }
+    },
+    events: {
+      scroll: function() {}
     },
     accessors: {
       "setupItem": {
-        get: function () {
+        get: function() {
           return this.xtag.setupItem;
         },
-        set: function (value) {
+        set: function(value) {
           this.xtag.setupItem = value;
         }
       },
       "count": {
-        get: function () {
+        get: function() {
           return this.xtag.count;
         },
-        set: function (value) {
+        set: function(value) {
           this.xtag.count = value;
         }
       }
-    }, 
+    },
     methods: {
       //Starts rendering the list.
-      render: function () {
+      render: function(top) {
         if (!setupItem || !count) return false;
-        this.xtag.topItem = undefined;
-        this.xtag.bottomItem = undefined;
+        this.xtag.topIndex = undefined;
+        this.xtag.bottomIndex = undefined;
         this.xtag.itemCount = 0;
-        this.xtag.upRender = undefined;
-        this.xtag.downRender = undefined;
+        this.xtag.extraRenderRangeUp = [undefined, undefined];
+        this.xtag.extraRenderRangeDown = [undefined, undefined];
         this.xtag.active = true;
+        while (this.itemInViewport(bottomIndex)) {
+
+        }
       },
+      destroy: function () {
+        this.destroyItems(0, this.count-1);
+      }
       //Renders a single Item
-      renderItem: function () {
+      renderItem: function(index) {
         if (!this.active) return false;
       },
       //Renders multiple Items
-      renderItems: function () {
-      },
-      destroyItem: function () {
-      },
+      renderItems: function(indexStart, indexEnd) {},
+      //Destroys a single Item
+      destroyItem: function(index) {},
       //Destroys multiple Items
-      destroyItems: function () {
-      }
+      destroyItems: function(indexStart, indexEnd) {}
       //Returns a single Item
-      getItem: function (index) {
-        return this.querySelector("[data-index="+index+"]");
+      getItem: function(index) {
+        return this.querySelector("[data-index=" + index + "]");
       },
       //Returns multiple Items as an array
       //Not sure why you'd need this
@@ -65,17 +70,35 @@
       //It's just here to replicate the experience of 
       //the other xxxxxItem functions having a 
       //doppleganger for multiple items.
-      getItems: function (indexStart, indexEnd) {
+      getItems: function(indexStart, indexEnd) {
         var array = [],
-        i = 0;
-        while(indexStart !== indexEnd+1) {
+          i = 0;
+        while (indexStart == < indexEnd) {
           array[i] = this.getItem(indexStart);
           indexStart++;
           i++;
         }
         return array;
       },
-    }
+      itemInViewport: function(index) {
+        var rect = el.getBoundingClientRect();
+        return (
+          rect.top >= 0 &&
+          rect.left >= 0 &&
+          rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /*or $(window).height() */
+          rect.right <= (window.innerWidth || document.documentElement.clientWidth) /*or $(window).width() */
+        );
+      },
+      itemsInViewport: function(indexStart, indexEnd) {
+        var result;
+        while (indexStart == < indexEnd) {
+          if (this.itemInViewport(indexStart)) return true;
+
+        }
+        return false;
+      }
+    },
+    mixins: ["prepend"]
   });
 
 }());
