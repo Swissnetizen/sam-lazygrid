d7d7068435b7e92810abd390216379ea6c224d20
Samarth AGARWAL <samu.agarwal@gmail.com>
2014-03-30 21:04:27 +0200

    fastdomify

--- src/lazygrid.js
+++ src/lazygrid.js
@@ -2,10 +2,12 @@
   "use strict";
   // Create your component here
   // http://x-tags.org/docs
-
   xtag.register('sam-lazygrid', {
     lifecycle: {
-      created: function() {},
+      created: function() {
+        this.xtag.scrollFuncX= window.pageXOffset;
+        this.xtag.scrollFuncY= window.pageYOffset;
+      },
       inserted: function() {},
       removed: function() {
         this.destroy();
@@ -13,7 +15,20 @@
       attributeChanged: function() {}
     },
     events: {
-      scroll: function() {}
+      scroll: function () {
+        var diffX=this.xtag.scrollFuncX-window.pageXOffset;
+        var diffY=this.xtag.scrollFuncY-window.pageYOffset;
+        if( diffX<0 ) {
+            // Scroll right
+        } else if( diffX>0 ) {
+            // Scroll left
+        } else if( diffY<0 ) {
+            // Scroll down
+        } else if( diffY>0 ) {
+            // Scroll up
+        } else return;
+        
+      }
     },
     accessors: {
       "setupItem": {
@@ -37,29 +52,53 @@
       //Starts rendering the list.
       render: function(top) {
         if (!setupItem || !count) return false;
-        this.xtag.topIndex = undefined;
-        this.xtag.bottomIndex = undefined;
-        this.xtag.itemCount = 0;
-        this.xtag.extraRenderRangeUp = [undefined, undefined];
-        this.xtag.extraRenderRangeDown = [undefined, undefined];
-        this.xtag.active = true;
-        while (this.itemInViewport(bottomIndex)) {
-
-        }
+        var x = this.xtag
+        x.topIndex = undefined;
+        x.bottomIndex = undefined;
+        x.itemCount = 0;
+        x.extraRenderRangeUp = [undefined, undefined];
+        x.extraRenderRangeDown = [undefined, undefined];
+        x.active = true;
+        fastdom.write(function() {
+          while (this.itemInViewport(bottomIndex)) {
+            this.renderItem();
+          }
+          x.extraItems = Math.floor(x.count/4);
+        }.bind(this));
       },
+      //Destroys everything except itself.
       destroy: function () {
         this.destroyItems(0, this.count-1);
       }
       //Renders a single Item
       renderItem: function(index) {
         if (!this.active) return false;
+        fastdom.write(function () {
+          
+        }.bind(this))
       },
       //Renders multiple Items
-      renderItems: function(indexStart, indexEnd) {},
+      renderItems: function(indexStart, indexEnd) {
+        //Allows you to use an array instead of two seperate
+        //Arguments
+        if (Array.isArray(argument[0])) {
+          indexStart = arguments[0][0]; 
+          indexEnd = arguments[0][1]
+        }
+      },
+
       //Destroys a single Item
       destroyItem: function(index) {},
       //Destroys multiple Items
-      destroyItems: function(indexStart, indexEnd) {}
+      destroyItems: function(indexStart, indexEnd) {
+        //Allows you to use an array instead of two seperate
+        //Arguments
+        if (Array.isArray(argument[0])) {
+          indexStart = arguments[0][0]; 
+          indexEnd = arguments[0][1]
+        }
+      }
+      
       //Returns a single Item
       getItem: function(index) {
         return this.querySelector("[data-index=" + index + "]");
@@ -71,6 +110,12 @@
       //the other xxxxxItem functions having a 
       //doppleganger for multiple items.
       getItems: function(indexStart, indexEnd) {
+        //Allows you to use an array instead of two seperate
+        //Arguments
+        if (Array.isArray(argument[0])) {
+          indexStart = arguments[0][0]; 
+          indexEnd = arguments[0][1]
+        }
         var array = [],
           i = 0;
         while (indexStart == < indexEnd) {
@@ -90,8 +135,14 @@
         );
       },
       itemsInViewport: function(indexStart, indexEnd) {
+        //Allows you to use an array instead of two seperate
+        //Arguments
+        if (Array.isArray(argument[0])) {
+          indexStart = arguments[0][0]; 
+          indexEnd = arguments[0][1]
+        }
         var result;
-        while (indexStart == < indexEnd) {
+        while (indexStart ==< indexEnd) {
           if (this.itemInViewport(indexStart)) return true;
 
         }
